01b0b37ba0f4928ab35531f44d1f3af5ce67787e
leek <leeked@gmail.com>
2011-05-10 11:08:31 -0400

    Initial Import

---
+++ example.js
@@ -0,0 +1,40 @@
+var net = require('net'),
+	querystring = require('querystring'),
+	EventSocket = require('./index');
+
+var server = net.createServer(function (socket) {
+	
+    // onConnect
+    socket.on("connect", function () {
+		var self = this;
+        console.log("Socket Connected!");
+        console.log("Current connections: %d\n", server.connections);
+	
+		this.eventsocket = new EventSocket(socket);
+		
+		this.eventsocket.connect();
+		this.eventsocket.on("connect", function(response){
+			console.log("Connected to FreeSWITCH!");
+			self.answer();
+			self.playback("/usr/local/freeswitch/sounds/en/us/callie/ivr/8000/ivr-welcome_to_freeswitch.wav");
+		});
+    })
+	
+    // onData
+    socket.on("data", function (data) {
+		this.eventsocket.processData(data);
+    });
+	
+    // onClose
+    socket.on("close", function (had_error) {
+        socket.end();
+        console.log("Closed\n");
+        console.log("--------------------------------------------\n");
+    });
+
+});
+
+server.listen(8084, "127.0.0.1", function () {
+    address = server.address();
+    console.log("Server started listening on %s:%d\n", address.address, address.port);
+});
