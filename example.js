edb68632ee715d83946df438b0d8b810267bdcd2
leek <leeked@gmail.com>
2011-05-10 11:39:46 -0400

    Fixing tabs/spaces

--- example.js
+++ example.js
@@ -1,28 +1,28 @@
 var net = require('net'),
-	querystring = require('querystring'),
-	EventSocket = require('./index');
+querystring = require('querystring'),
+EventSocket = require('./index');
 
 var server = net.createServer(function (socket) {
 	
     // onConnect
     socket.on("connect", function () {
-		var self = this;
+        var self = this;
         console.log("Socket Connected!");
         console.log("Current connections: %d\n", server.connections);
 	
-		this.eventsocket = new EventSocket(socket);
+        this.eventsocket = new EventSocket(socket);
 		
-		this.eventsocket.connect();
-		this.eventsocket.on("connect", function(response){
-			console.log("Connected to FreeSWITCH!");
-			self.answer();
-			self.playback("/usr/local/freeswitch/sounds/en/us/callie/ivr/8000/ivr-welcome_to_freeswitch.wav");
-		});
+        this.eventsocket.connect();
+        this.eventsocket.on("connect", function(response){
+            console.log("Connected to FreeSWITCH!");
+            self.answer();
+            self.playback("/usr/local/freeswitch/sounds/en/us/callie/ivr/8000/ivr-welcome_to_freeswitch.wav");
+        });
     })
 	
     // onData
     socket.on("data", function (data) {
-		this.eventsocket.processData(data);
+        this.eventsocket.processData(data);
     });
 	
     // onClose
